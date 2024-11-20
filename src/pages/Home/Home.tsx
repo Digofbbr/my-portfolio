9feee66f2f90dcaababfc5c44c50401514927ae7
Rodrigo Barreto <rodrigofb06@gmail.com>
2024-11-20 20:19:28 -0300

    typping text on hero section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,19 @@
-import Navbar from "../../components/Navbar"
-import About from "./sections/About/About"
-import Hero from "./sections/Hero/Hero"
-import Projects from "./sections/Projects/Projects"
+import Navbar from "../../components/Navbar";
+import About from "./sections/About/About";
+import Contact from "./sections/Contact/Contact";
+import Hero from "./sections/Hero/Hero";
+import Projects from "./sections/Projects/Projects";
 
 const Home = () => {
-  
-  return (
-    <div>
-      <Navbar />
-      <Hero/>
-      <About/>
-      <Projects />
-    </div>
-  )
-}
+	return (
+		<div>
+			<Navbar />
+			<Hero />
+			<About />
+			<Projects />
+			<Contact />
+		</div>
+	);
+};
 
-export default Home
+export default Home;
